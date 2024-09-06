dacc6efe10a25359a95a438b2ba93dca56c32b10
KmancusoRonik <105605229+KmancusoRonik@users.noreply.github.com>
2024-09-06 18:33:57 -0400

    2.0.1.0.6
    fix reset api

--- public/rest-api/serviceworker-endpoint.php
+++ public/rest-api/serviceworker-endpoint.php
@@ -169,4 +169,5 @@ function ronikdesigns_service_worker_data( $data ) {
 register_rest_route( 'serviceworker/v1', '/data/(?P<slug>\w+)', array(
     'methods' => 'GET',
     'callback' => 'ronikdesigns_service_worker_data',
+    'permission_callback' => '__return_true', // Allows public access
 ));
